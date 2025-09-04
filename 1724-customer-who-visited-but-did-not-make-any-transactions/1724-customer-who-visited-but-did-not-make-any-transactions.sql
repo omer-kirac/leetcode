ba8f630da7eaca30a34bb3f342a481c62dafb529
omer-kirac <80053889+omer-kirac@users.noreply.github.com>
2025-09-04 19:16:18 +0300

    Time: 1266 ms (36.65%), Space: 0B (100%) - LeetHub

--- 1724-customer-who-visited-but-did-not-make-any-transactions/1724-customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/1724-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,7 +1,8 @@
-/* Write your PL/SQL query statement below */
-SELECT Visits.customer_id,COUNT(Visits.visit_id) AS count_no_trans 
-FROM Visits
-LEFT JOIN Transactions ON Visits.visit_id = Transactions.visit_id
-WHERE Transactions.visit_id IS NULL
-GROUP BY Visits.customer_id
-ORDER BY count_no_trans ASC
+SELECT v.customer_id,
+       COUNT(v.visit_id) AS count_no_trans
+FROM Visits v
+LEFT JOIN Transactions t
+       ON v.visit_id = t.visit_id
+WHERE t.visit_id IS NULL
+GROUP BY v.customer_id
+ORDER BY count_no_trans ASC;
